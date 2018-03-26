b853a1076ef278bb54d2d86a30edd66adc8df6a6
Liyan <liyan@liyanchua.com>
2018-03-26 17:32:54 +0800

    Update email

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,9 @@ class LikesController < ApplicationController
 		@note = Note.find(params[:note_id])
 		note_likes = @note.likes.build
 		note_likes.user = current_user
-		note_likes.save
+		if note_likes.save
+			UserMailer.new_like_email(current_user).deliver_later
+		end
   end
   
   def destroy
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@notes.com'
 
-  def new_follower(user)
+  def new_like_email(user)
     @email = user.email
     mail to: user.email, subject: "You have a new follower!"
   end
