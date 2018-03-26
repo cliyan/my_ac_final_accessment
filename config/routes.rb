2d2312736f938f938b82655dcdbe81aa63cf6796
Liyan <liyan@liyanchua.com>
2018-03-26 11:32:05 +0800

    Create notes model and controller, update routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: "notes#index"
+
+  resources :notes
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :relationships, only: [:create, :destroy]
+    member do
+      get :followings, :followers
+    end
+  end
+  
 end
